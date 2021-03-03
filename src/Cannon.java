c344fdcc55c35d59d1a657ad187e67d721d631c5
alyakrinskiyam.22 <sasha9andrey@gmail.com>
2021-03-03 12:15:31 +0300

    initial

---
+++ src/Cannon.java
@@ -0,0 +1,66 @@
+import java.util.LinkedList;
+import java.util.Random;
+
+import static java.lang.Math.*;
+
+public class Cannon {
+
+    private double y = 0.5;
+    private static final double speed = 0.015;
+    private final int reloadTime = 10;
+    private int reload = reloadTime;
+
+    private static final Random rnd = new Random();
+    private int bulletType;
+
+    public boolean isBonus = false; //TODO: public to private
+
+    boolean up = false, down = false, jump = false;
+
+    public Cannon(int type) {
+        bulletType = type;
+    }
+
+    public void update() {
+        if (reload < reloadTime)//TODO: remove fps correlation
+            reload++;
+        int direction = (up ? 1 : 0) - (down ? 1 : 0);
+        if (jump) {
+            if (direction != 0)
+                y = 0.5 - direction / 2.0;
+            return;
+        }
+
+        y -= speed * direction; //TODO: remove fps correlation
+        y = max(0.0, min(y, 1.0));
+    }
+
+    public void shoot(LinkedList<Bullet> bullets, int type, LinkedList<Enemy> enemies) {
+        if (reload == reloadTime && bulletType == type || isBonus) {
+            bullets.add(new Bullet(0, y, bulletType));
+            bulletType = rnd.nextInt(3);
+            while (!checkNewBullet(enemies) && enemies.size() != 0)
+                bulletType = (bulletType + 1) % 3;
+            reload = 0;
+        }
+    }
+
+    private boolean checkNewBullet(LinkedList<Enemy> enemies) {
+        boolean res = false;
+        for (Enemy enemy : enemies) {
+            if (enemy.getType() == bulletType) {
+                res = true;
+                break;
+            }
+        }
+        return res;
+    }
+
+    public double getPos() {
+        return y;
+    }
+
+    public int getBulletType() {
+        return bulletType;
+    }
+}
---
+++ src/Game.java
@@ -0,0 +1,109 @@
+import java.util.LinkedList;
+import java.util.Random;
+
+public class Game {
+
+    private static final int enemyRate = 90;
+    private static final int enemyRateMaxDiff = 70;
+    private int enemyReload = 0;
+    private int enemyRateDiff = 0;
+    private int rowScore = 0, score = 0, bonusReload = 0;
+    private boolean isBonus = false;
+
+    private boolean is_run = true;
+
+    private static final Random rnd = new Random();
+
+    LinkedList<Bullet> bullets = new LinkedList<>();
+    LinkedList<Enemy> enemies = new LinkedList<>();
+    Cannon cannon;
+
+    public Game() {
+        enemies.add(new Enemy());
+        cannon = new Cannon(enemies.getFirst().getType());
+    }
+
+    public LinkedList<Enemy> getEnemies() {
+        return enemies;
+    }
+
+    public void update() {
+        if (!is_run)
+            return;
+        if (isBonus)
+        {
+            bonusReload++;
+            if (bonusReload == 1000) {
+                bonusReload = 0;
+                isBonus = false;
+            }
+        }
+        if (enemyReload + enemyRateDiff < enemyRate)
+            enemyReload++;
+        else {
+            enemies.add(new Enemy());
+            enemyReload = 0;
+            enemyRateDiff = rnd.nextInt(enemyRateMaxDiff) - enemyRateMaxDiff / 2;
+        }
+        for (Enemy enemy : enemies) {
+            if (!enemy.move()) {
+                is_run = false;
+                break;
+            }
+        }
+        cannon.isBonus = isBonus;
+//TODO: speedup
+        cannon.update();
+        bullets.removeIf(this::collisionCheck);
+        bullets.removeIf(bullet -> !bullet.move());
+    }
+
+    public void run() {
+        Frame frame = new Frame(this);
+    }
+
+    private boolean collisionCheck(Bullet bullet) {
+        for (Enemy enemy : enemies) {
+            if (enemy.screenPos.intersects(bullet.screenPos)) {
+                if (enemy.getType() == bullet.getType() || isBonus) {
+                    enemies.remove(enemy);
+                    rowScore++;
+                    score++;
+                    if (!isBonus) {
+                        bonusReload++;
+                        if (bonusReload == 5) {//TODO: remove number
+                            bonusReload = 0;
+                            isBonus = true;
+                        }
+                    }
+                } else {
+                    rowScore = 0;
+                }
+                System.out.print("Score in a row: ");
+                System.out.println(rowScore);
+                System.out.print("Score: ");
+                System.out.println(score);
+                clearConsole();
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void clearConsole() //TODO: Don't work on linux from Intellij
+    {
+        try
+        {
+            final String os = System.getProperty("os.name");
+
+            if (os.contains("Windows"))
+                Runtime.getRuntime().exec("cls");
+            else
+                Runtime.getRuntime().exec("clear");
+        }
+        catch (final Exception e) {
+            System.out.println("Use normal OS");
+        }
+    }
+}
